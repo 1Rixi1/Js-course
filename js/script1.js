e491d34c27b769b3c6e31cd3075161fb12631c9d
Maksim <grigoriev.ma2014@yandex.ru>
2022-03-01 21:13:54 +0300

    practice-3

---
+++ js/script1.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+// console.log(str[2] = 'd');
+// console.log(str);
+
+const str = 'tesT';
+
+// console.log(str.toLocaleUpperCase());
+
+// console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello World';
+
+console.log(logg.substr(6, 5));
+
+
+// console.log(logg.slice(-5, -1));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
